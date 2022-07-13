35325cab99d28c5b9d799f85c1e01731add184cf
Amit Abdullin <amirop35@gmail.com>
2022-07-13 15:19:02 +0300

    fix/new migration API template require path

--- templates/migration-template.js
+++ templates/migration-template.js
@@ -1,4 +1,4 @@
-const { createMigration } = require('aurora-orm/src/migrator/templates/v1')
+const { createMigration } = require('aurora-orm/dist/migrator/templates/v1')
 
 module.exports = createMigration({
   async up({ db }) {
